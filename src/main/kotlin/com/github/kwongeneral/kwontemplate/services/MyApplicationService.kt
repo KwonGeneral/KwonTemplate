c15b839edd4ec1c0b639f9c2f3ffeef9c2b6733f
GitHub Action <action@github.com>
2022-03-15 03:10:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/kwongeneral/kwontemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kwongeneral/kwontemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kwongeneral.kwontemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kwongeneral.kwontemplate.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/kwongeneral/kwontemplate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kwongeneral/kwontemplate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kwongeneral.kwontemplate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kwongeneral.kwontemplate.MyBundle
 
 class MyProjectService(project: Project) {
 
